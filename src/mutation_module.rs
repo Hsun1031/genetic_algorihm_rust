1c1cedaacbf97ac19fc3a8955c788510b50f4f8f
Hsun <hsun1031@outlook.com>
2023-11-21 11:05:24 +0000

    :bug: fix: Fix probability bug in mutation module

--- src/mutation_module.rs
+++ src/mutation_module.rs
@@ -6,12 +6,14 @@ pub fn chromosome() {
 
 pub fn gene(bits_list: &Vec<[u8; 24]>) -> Vec<[u8; 24]> {
     let mut rng:ThreadRng = thread_rng();
-    let my_probability = 0.03 / (bits_list.len() as f64);
+    let my_probability = 0.03 / (bits_list[0].len() as f64);
+    // println!("my_probability: {}", my_probability);
 
     let mut new_bits_list = bits_list.clone();
 
     for i in 0..bits_list.len() {
         let mut new_bits = bits_list[i].clone();
+
         match bits_list.get(i) {
             Some(bits) => {
                 for j in 0..bits.len() {
@@ -20,15 +22,15 @@ pub fn gene(bits_list: &Vec<[u8; 24]>) -> Vec<[u8; 24]> {
                     if num < my_probability {
                         // println!("i: {}, num: {}, my_probability: {}", i, num, my_probability);
                         new_bits[j] = if bits[j] == 0 { 1 } else { 0 };
-                        new_bits_list[i] = new_bits;
                     };
-                
                 }
             },
             None => println!("Error: bits_list.get({}) is None", i),
         }
-        // println!("   bits_list: {:?}", bits_list[i]);
-        // println!("new_bits_list:{:?}", new_bits_list);
+
+        new_bits_list[i] = new_bits;
+        // println!("    bits_list: {:?}", bits_list[i]);
+        // println!("new_bits_list: {:?}", new_bits_list[i]);
     }
 
     new_bits_list
