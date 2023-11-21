7336b6ff4b2300711dd448be0f6e91a99f1c3dff
Hsun <hsun1031@outlook.com>
2023-11-21 10:40:13 +0000

    :bug: fix: Fix the bug of crossover module

--- src/mutation_module.rs
+++ src/mutation_module.rs
@@ -14,12 +14,12 @@ pub fn gene(bits_list: &Vec<[u8; 24]>) -> Vec<[u8; 24]> {
         let mut new_bits = bits_list[i].clone();
         match bits_list.get(i) {
             Some(bits) => {
-                for i in 0..bits.len() {
+                for j in 0..bits.len() {
                     let num: f64 = rng.gen(); // generates a float between 0 and 1
 
                     if num < my_probability {
                         // println!("i: {}, num: {}, my_probability: {}", i, num, my_probability);
-                        new_bits[i] = if bits[i] == 0 { 1 } else { 0 };
+                        new_bits[j] = if bits[j] == 0 { 1 } else { 0 };
                         new_bits_list[i] = new_bits;
                     };
                 
