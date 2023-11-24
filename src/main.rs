19c20cb2b1d89d7d0721469b9194380b1a6bb418
Hsun <hsun1031@outlook.com>
2023-11-24 12:31:54 +0000

    ✨ feat: Add std_deviation

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ mod crossover_module;
 mod mutation_module;
 
 use std::fs;
-
-// use std::time::Instant;
+use std::iter::Iterator;
 
 fn main() {
     let len = 1000;
     let mut max_list:Vec<i16> = Vec::new();
-    let mut avg_list:Vec<f64> = Vec::new();
+    let mut avg_list:Vec<f32> = Vec::new();
+    let mut std_deviation_list:Vec<f32> = Vec::new();
 
     let (mut bits_list, mut output_list ) = init_population(len);
 
@@ -20,9 +20,10 @@ fn main() {
         let (mutation_bits_list, mutation_output_list)  = mutation(&cross_bits_list);
 
         // find max and avg
-        let (max, avg) = get_max_and_avg(&mutation_output_list);
+        let (max, avg, std_deviation) = get_output(&mutation_output_list);
         max_list.push(max);
         avg_list.push(avg);
+        std_deviation_list.push(std_deviation);
 
         bits_list   = mutation_bits_list.  clone();
         output_list = mutation_output_list.clone();
@@ -31,9 +32,10 @@ fn main() {
     // println!("avg_list: {:?}", avg_list);
     let max_list_str = max_list.iter().map(|i| i.to_string()).collect::<Vec<String>>().join(", ");
     let avg_list_str = avg_list.iter().map(|i| i.to_string()).collect::<Vec<String>>().join(", ");
+    let std_deviation_list_str = std_deviation_list.iter().map(|i| i.to_string()).collect::<Vec<String>>().join(", ");
 
     // Write the strings to files
-    fs::write("data.js", format!("const max_list = [{}];\nconst avg_list = [{}];", max_list_str, avg_list_str)).expect("Unable to write file");
+    fs::write("data.js", format!("const max_list = [{}];\nconst avg_list = [{}];\nconst std_deviation_list = [{}]", max_list_str, avg_list_str, std_deviation_list_str)).expect("Unable to write file");
 
     println!("-------------------------------------------------------");
     println!("Done!");
@@ -125,26 +127,37 @@ fn mutation(bits_list: &Vec<[u8; 24]>) -> (Vec<[u8; 24]>, Vec<i16>) {
     (new_bits_list, new_output_list)
 }
 
-fn get_max_and_avg(output_list: &Vec<i16>) -> (i16, f64) {
-    let mut total: i32 = 0;
+fn mean(data: &[i16]) -> Option<f32> {
+    let sum = data.iter().map(|&value| value as i128).sum::<i128>() as f32;
+    let count = data.len();
 
-    let mut max = 0;
-
-    for i in 0..output_list.len() {
-        match output_list.get(i) {
-            Some(output) => {
-                if max < *output {
-                    max = *output;
-                }
-                total += *output as i32;
-            },
-            None => println!("Error: output_list.get({}) is None", i),
-        }
+    match count {
+        positive if positive > 0 => Some(sum / count as f32),
+        _ => None,
     }
+}
 
-    let avg: f64 = ( total as f64/ (output_list.len() as f64) ) as f64;
+fn std_deviation_fn(data: &[i16]) -> Option<f32> {
+    match (mean(data), data.len()) {
+        (Some(data_mean), count) if count > 0 => {
+            let variance = data.iter().map(|value| {
+                let diff = data_mean - (*value as f32);
 
-    (max, avg)
+                diff * diff
+            }).sum::<f32>() / count as f32;
+
+            Some(variance.sqrt())
+        },
+        _ => None
+    }
+}
+
+fn get_output(output_list: &[i16]) -> (i16, f32, f32) {
+    // Option<f32> to f32
+    let _max = output_list.iter().max().unwrap_or(&0);
+    let _mean = mean(output_list).unwrap_or(0.0);
+    let _std_deviation = std_deviation_fn(output_list).unwrap_or(0.0);
+    (*_max, _mean , _std_deviation)
 }
 
 // // fn reporting() {
